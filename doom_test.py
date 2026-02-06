b4eaef29cb760eeab47568412eecb95055964a9b
Sebastian Tenorio <sebas.tenorio2311@gmail.com>
2026-02-06 16:21:06 -0500

    Add: Training switched to PPO Reinforcement Learning model

--- doom_test.py
+++ doom_test.py
@@ -1,93 +1,69 @@
-import torch
-import torch.nn as nn
-import cv2
-import numpy as np
 import vizdoom as vzd
+import numpy as np
+import cv2
 import time
+from stable_baselines3 import PPO
 
-# Arquitectura (parecida a la de entrenamiento)
+MODEL_PATH = "doom_ppo_final" 
 
-class DoomAgentNet(nn.Module):
-    def __init__(self, input_h, input_w, num_actions):
-        super(DoomAgentNet, self).__init__()
-        input_size = input_h * input_w 
-        
-        self.net = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(input_size, 512),
-            nn.ReLU(),
-            nn.Linear(512, 128),
-            nn.ReLU(),
-            nn.Linear(128, num_actions)
-        )
+CONFIG_PATH = "scenarios/Amalgoom.cfg" 
 
-    def forward(self, x):
-        return self.net(x)
+#! Debe ser IDÉNTICA a la que usaste en la clase VizDoomGym
+ACTIONS_LIST = [
+    [0, 0, 0, 0, 0, 0], # 0. Quieto
+    [1, 0, 0, 0, 0, 0], # 1. Avanzar
+    [0, 1, 0, 0, 0, 0], # 2. Girar Izq
+    [0, 0, 1, 0, 0, 0], # 3. Girar Der
+    [0, 0, 0, 1, 0, 0], # 4. Disparar
+    [0, 0, 0, 0, 1, 0]  # 5. ABRIR PUERTA (USE)
+]
 
-# Configuracion
+print(f"Cargando modelo desde {MODEL_PATH}...")
 
-MODEL_PATH = "doom_model_gpu.pth"  # Modelo entrenado
-CONFIG_PATH = "scenarios/D3_battle.cfg" # Escenario entrenado
-NUM_ACCIONES = 14  # Debe coincidir con el entrenamiento
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Corriendo inferencia en: {device}")
-
-# Inicializamos la red vacía (64x64 es lo que se usó)
-model = DoomAgentNet(64, 64, NUM_ACCIONES).to(device)
-
-# Cargamos los pesos entrenados
-model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
-model.eval() # Poner en modo evaluación (congela dropout, etc)
+model = PPO.load(MODEL_PATH)
 print("¡Cerebro cargado correctamente!")
 
-# Inicia el juegoo
 game = vzd.DoomGame()
 game.load_config(CONFIG_PATH)
-game.set_window_visible(True) # Para visualización
-game.set_mode(vzd.Mode.ASYNC_PLAYER) # Modo jugador (no entrenamiento)
+game.set_window_visible(True)  
+game.set_mode(vzd.Mode.ASYNC_PLAYER) 
 game.init()
 
 print("Iniciando partida... Presiona Ctrl+C para parar.")
 
 episodios = 5
+
 for i in range(episodios):
     game.new_episode()
+    
     while not game.is_episode_finished():
         state = game.get_state()
-        
-        # preprocesar (obtenemos imagen original)
-        frame = state.screen_buffer # Viene como (3, H, W) o (H, W, 3) dependiendo config
-        
-        if frame.shape[0] == 3: 
+        frame = state.screen_buffer # Viene (3, H, W) o (H, W, 3)
+
+        #! PREPROCESAMIENTO: Debe ser IGUAL al de VizDoomGym)
+        # formato para opencv (H, W, Canales)
+        if frame.shape[0] == 3:
             frame = np.transpose(frame, (1, 2, 0))
-            
-        # Resize a 64x64 y Grayscale
+        
         frame = cv2.resize(frame, (64, 64))
+        
         if len(frame.shape) == 3 and frame.shape[2] == 3:
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
             
-        # Normalizar y Tensor
-        frame_tensor = torch.from_numpy(frame).float() / 255.0
-        frame_tensor = frame_tensor.unsqueeze(0).unsqueeze(0) # (1 batch, 1 canal, 64, 64)
-        frame_tensor = frame_tensor.to(device)
+        frame = frame[:, :, np.newaxis]
 
-        # INFERENCIA
-        with torch.no_grad(): 
-            outputs = model(frame_tensor)
-            
-            # El modelo devuelve valores continuos (0.02, 0.95, -0.1)
-            # Necesitamos convertirlos a botones (0 o 1)
-            # Usamos un umbral (threshold) de 0.5
-            prediction = (outputs > 0.5).float().cpu().numpy()[0]
-            
-            action = prediction.astype(int).tolist()
+        #! Inferenciaa
+        action_index, _ = model.predict(frame, deterministic=True)
 
-        # Action!
-        game.make_action(action)
+        print(f"Predicción: {action_index}")
         
-        time.sleep(0.02) 
+        action_buttons = ACTIONS_LIST[int(action_index)]
 
-    print(f"Episodio {i+1} terminado.")
+        #! Actuaar
+        game.make_action(action_buttons)
+        
+        time.sleep(0.03) 
+
+    print(f"Episodio {i+1} terminado. Score: {game.get_total_reward()}")
 
 game.close()
