ae0a700ac2ae4efa16b5bcfd3183de7ea6de2743
Sebastian Tenorio <sebas.tenorio2311@gmail.com>
2026-03-02 14:41:51 -0500

    Fix: config file for rocket_basic

--- doom_test.py
+++ doom_test.py
@@ -5,7 +5,7 @@ import time
 from collections import deque 
 from stable_baselines3 import PPO
 
-MODEL_PATH = "doom_ppo_assassin" 
+MODEL_PATH = "doom_ppo_final" 
 CONFIG_PATH = "scenarios/deadly_corridor.cfg"
 
 #! Debe ser IDÉNTICA a la de la clase VizDoomGym
